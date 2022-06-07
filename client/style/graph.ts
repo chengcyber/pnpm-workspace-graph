acfca40337f748d44380c9a883db9211f16d9140
Cheng Liu <chengcyber@outlook.com>
2022-06-07 09:50:22 +0800

    :lipstick: palette

--- client/style/graph.ts
+++ client/style/graph.ts
@@ -1,4 +1,5 @@
 import type cy from "cytoscape";
+import { palette } from "./palette";
 
 export const cyGraphStyle: cy.Stylesheet[] = [
   {
@@ -11,15 +12,22 @@ export const cyGraphStyle: cy.Stylesheet[] = [
       width: (node: any) => {
         return node.data("id").length * 16;
       },
+      color: palette.black,
+      "border-width": "1px",
+      "border-style": "solid",
+      "border-color": palette.neutralTertiary,
+      "background-color": palette.themeLighter,
     },
   },
   {
     selector: "edge",
     style: {
-      width: "2px",
+      width: "1px",
+      "line-color": palette.neutralTertiary,
       "curve-style": "unbundled-bezier",
       "target-arrow-shape": "triangle",
       "target-arrow-fill": "filled",
+      "target-arrow-color": palette.neutralQuaternary,
     },
   },
 ];
---
+++ client/style/palette.ts
@@ -0,0 +1,4 @@
+import { DefaultPalette, IPalette } from "@fluentui/react";
+export const palette: IPalette = {
+  ...DefaultPalette,
+};
